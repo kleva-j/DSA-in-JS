a5d161e7ce1ea85ef96a212a8f43e41df1d7e0d9
kleva-j <kasmickleva@gmail.com>
2025-05-13 13:09:32 +0100

    chore: setup shadcn with tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react';
+import React from 'react';
+
 import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx';
+import App from './App';
 
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/dsa-in-js/',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [tsconfigPaths(), react(), tailwindcss()],
 });
