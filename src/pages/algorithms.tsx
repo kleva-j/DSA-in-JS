3cf868ffe23342b1e26e783ce47bb405d1fd6aec
kleva-j <kasmickleva@gmail.com>
2025-05-23 10:22:48 +0100

    feat: analyse filesystem and render a treeview structure of the algorithms dir in its page

--- src/pages/algorithms.tsx
+++ src/pages/algorithms.tsx
@@ -3,41 +3,6 @@ import type { TreeViewItem } from '@/components/molecule/tree-item';
 import { Globe, Folder, FolderOpen, File, Download } from 'lucide-react';
 import { TreeView } from '@/components/molecule/tree';
 
-const data = [
-  {
-    id: '1',
-    name: 'Algorithms',
-    type: 'region',
-    children: [
-      {
-        id: '1.1',
-        name: 'Sorting',
-        type: 'store',
-        children: [
-          {
-            id: '1.1.1',
-            name: 'Bubble Sort',
-            type: 'department',
-            children: [
-              { id: '1.1.1.1', name: 'Bubble Sort', type: 'item' },
-              { id: '1.1.1.2', name: 'Quick Sort', type: 'item' },
-            ],
-          },
-          {
-            id: '1.1.2',
-            name: 'Searching',
-            type: 'department',
-            children: [
-              { id: '1.1.2.1', name: 'Binary Search', type: 'item' },
-              { id: '1.1.2.2', name: 'Linear Search', type: 'item' },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-];
-
 const customIconMap = {
   region: <Globe className="h-4 w-4 text-purple-500" />,
   store: <Folder className="h-4 w-4 text-blue-500" />,
@@ -54,6 +19,72 @@ const menuItems = [
   },
 ];
 
+const levelMap: Record<number, string> = {
+  0: 'region',
+  1: 'store',
+  2: 'department',
+  3: 'item',
+};
+
+type TreeItem = {
+  id: string;
+  name: string;
+  type: string;
+  children?: TreeItem[];
+};
+
+function genTreeNodeId(parentId: string, index: number) {
+  return index === 0 ? `${parentId}` : `${parentId}.${index}`;
+}
+
+function generateTreeDfs(node: TreeItem, pathParts: string[], index: number) {
+  if (pathParts.length === 0 || index >= pathParts.length) return;
+
+  const name = pathParts[index];
+  const hasReachedEnd = index === pathParts.length - 1;
+  const isFile =
+    (hasReachedEnd && pathParts[index].endsWith('.ts')) ||
+    pathParts[index].endsWith('.tsx') ||
+    pathParts[index].endsWith('.mdx');
+  const children = node.children ?? [];
+  const child = children.find((child: TreeItem) => child.name === name);
+
+  if (!child) {
+    const id = genTreeNodeId(node.id, children.length + 1);
+
+    const newNode = { id, name, type: isFile ? 'item' : levelMap[index + 1] } as TreeItem;
+    if (!hasReachedEnd) newNode.children = [];
+
+    children.push(newNode);
+
+    generateTreeDfs(newNode, pathParts, index + 1);
+  } else {
+    generateTreeDfs(child, pathParts, index + 1);
+  }
+}
+
+type Module = { [key: string]: { default: unknown } };
+
+function loadModuleAndGenerateTreeView() {
+  const module = import.meta.glob('../algorithms/**/*.{ts,tsx,mdx}', { eager: true }) as Module;
+
+  const tree = {
+    id: genTreeNodeId('1', 0),
+    name: 'Algorithms',
+    children: [],
+    type: levelMap[0],
+  };
+
+  for (const path in module) {
+    const modulePath = path.split('/').slice(2);
+    generateTreeDfs(tree, modulePath, 0);
+  }
+
+  return tree;
+}
+
+const modules = loadModuleAndGenerateTreeView();
+
 export const AlgorithmsPageRoute = () => {
   const handleCheckChange = (item: TreeViewItem, checked: boolean) => {
     console.log(`Item ${item.name} checked:`, checked);
@@ -61,11 +92,11 @@ export const AlgorithmsPageRoute = () => {
 
   return (
     <TreeView
-      data={data}
-      title="Algorithms"
-      menuItems={menuItems}
-      iconMap={customIconMap}
       onCheckChange={handleCheckChange}
+      data={[modules]}
+      iconMap={customIconMap}
+      menuItems={menuItems}
+      title="Algorithms"
     />
   );
 };
